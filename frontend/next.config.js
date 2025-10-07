531ef8e4a95c1813c59490643afe9df953d26c11
daniilvasilevski-lab <daniilvasilevski@gmail.com>
2025-10-07 15:48:31 +0200

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,1 +1,15 @@
+/** @type {import('next').NextConfig} */
+const { i18n } = require('./next-i18next.config.js')
+
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  i18n,
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL,
+  }
+}
+
+module.exports = nextConfig
 
